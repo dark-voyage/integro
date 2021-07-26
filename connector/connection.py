380fa5ce803de5caa4ae716f43fa8ec3f836c169
Asrorbek-Orzikulov <asrorbek.arzikulov@gmail.com>
2021-07-26 11:01:01 +0500

    Removing phone number.
    Removing unneccessary phone number, which is not used anywhere in the code.

--- connector/connection.py
+++ connector/connection.py
@@ -9,8 +9,6 @@ config.read("config.ini")
 # Setting configuration values
 api_id = int(config['Integro']['api_id'])
 api_hash = str(config['Integro']['api_hash'])
-
-phone = config['Account']['phone']
 username = config['Account']['username']
 
 
